5922b99b9cfba54ff06d6b0c578fd6ac2ca30649
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2023-01-09 12:26:53 +0600

    jsjsjsjsj

--- 1-object-create.js
+++ 1-object-create.js
@@ -1,6 +1,26 @@
-// using object literal
+//1 using object literal
 const student = {name: 'jahid', job: 'cricketer'};
 // constructor
 const person = new Object();
-//
+//2
 console.log(person)
+//3
+//const  human = Object.create(null);
+const  human = Object.create(student);
+console.log(human.job);
+
+class people {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+}
+const peo = new people('manush', 12);
+console.log(peo);
+
+//function
+function manush(name){
+  this.name = name;
+}
+const man = new manush('shaharin');
+console.log(man);
