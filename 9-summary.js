3525b59e6b2bd35e769750edab6848ded3dc595e
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2023-01-17 17:29:23 +0600

    jjjsjuwwuwu

---
+++ 9-summary.js
@@ -0,0 +1,10 @@
+const bottle = {color: 'yellow', hold: 'water', price: 50, isCleaned: true};
+for (const key in bottle){
+  console.log(bottle[key])
+}
+
+const a = {a: 1};
+const b = {b: 1};
+const c = a;
+
+//ques: diffences between arrow function vs normal function
